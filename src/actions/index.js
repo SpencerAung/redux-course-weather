ac2d7c032ae5fc2a3a66c796ed2d6309d6ab1f6a
susuaung <susuaung.ms@gmail.com>
2017-03-13 16:07:08 +0900

    Redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '90e30a8470927741ee7ef358a82c7ca5';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -9,6 +9,8 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},mm`;
   const request = axios.get(url);
 
+  console.log('Request:', request);
+  
   return {
     type: FETCH_WEATHER,
     payload: request
